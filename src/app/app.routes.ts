ddce2304b4adfb60601fb71f516678bbe88ea2ce
geeta-patel-2 <gpatel2@student.touro.edu>
2024-09-11 23:34:10 -0400

    all content has been included with in the HTML files css file partially updated according to current design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,16 @@ import { Routes } from '@angular/router';
 import {HomePageComponent} from "./components/home-page/home-page.component";
 import {AboutPageComponent} from "./components/about-page/about-page.component";
 import {ContactPageComponent} from "./components/contact-page/contact-page.component";
+import {MeetTheChefComponent} from "./components/meet-the-chef/meet-the-chef.component";
 import {PhotoGalleryComponent} from "./components/photo-gallery/photo-gallery.component";
 import {FranchiseeComponent} from "./components/franchisee/franchisee.component";
-import {MeetTheChefComponent} from "./components/meet-the-chef/meet-the-chef.component";
 import {OurMenusComponent} from "./components/our-menus/our-menus.component";
 
 export const routes: Routes = [
   {path: '', component: HomePageComponent},
   {path: 'about-us', component: AboutPageComponent},
   {path: 'our-menus', component: OurMenusComponent},
-  {path: 'meet-the-chef/', component: MeetTheChefComponent},
+  {path: 'meet-the-chef', component: MeetTheChefComponent},
   {path: 'photo-gallery', component: PhotoGalleryComponent},
   {path: 'franchisee', component: FranchiseeComponent},
   {path: 'contact-us', component: ContactPageComponent}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+    imports: [
+        RouterLink,
+        NgOptimizedImage
+    ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
