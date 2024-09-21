384b1c3c78db3cc0b9e53bd20d68726c5c62b540
geeta-patel-2 <gpatel2@student.touro.edu>
2024-09-21 13:22:37 -0400

    CSS and JS adjusted. Added video and fonts.

--- src/app/components/about-page/about-page.component.ts
+++ src/app/components/about-page/about-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule, NgOptimizedImage],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss'
 })
 export class AboutPageComponent {
-
+  //images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, inject, TemplateRef} from '@angular/core';
+import {NgbCarousel, NgbModal, NgbSlide} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    NgbCarousel,
+    NgbSlide
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
+
+  public modalService = inject(NgbModal);
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
 
 }
