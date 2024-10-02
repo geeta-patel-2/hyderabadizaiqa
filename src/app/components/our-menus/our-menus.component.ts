e75df2037be4a6a4a59325d60f44f10fbb8b8799
geeta-patel-2 <gpatel2@student.touro.edu>
2024-10-01 21:05:12 -0400

    menu details link change in our menu page 3

--- src/app/components/our-menus/our-menus.component.ts
+++ src/app/components/our-menus/our-menus.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-our-menus',
   standalone: true,
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './our-menus.component.html',
   styleUrl: './our-menus.component.scss'
 })
