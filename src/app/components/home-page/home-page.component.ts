a31925fa4483033c2e0b2aa547cd00e894a2d406
geeta-patel-2 <gpatel2@student.touro.edu>
2024-11-25 00:14:30 -0500

    11/24/2024 Build 2.

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,18 +1,17 @@
 import {Component, inject} from '@angular/core';
 import {NgbCarousel, NgbModal, NgbSlide} from "@ng-bootstrap/ng-bootstrap";
 import {RouterLink} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
     selector: 'app-home-page',
     standalone: true,
-    imports: [
-        NgbCarousel,
-        NgbSlide,
-        RouterLink
-    ],
+    imports: [NgbCarousel, NgbSlide, RouterLink, NgOptimizedImage],
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
     public modalService = inject(NgbModal);
+
+    images: Array<String> = ["/assets/images/banner%20Logo.jpg"];
 }
