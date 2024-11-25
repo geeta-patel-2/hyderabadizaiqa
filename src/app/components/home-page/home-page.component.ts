160baf217831216f9408ee9e21d22aeacc5697f0
geeta-patel-2 <gpatel2@student.touro.edu>
2024-11-24 23:51:31 -0500

    responsive 2 done

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,20 +1,18 @@
-import {Component, inject, TemplateRef} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {NgbCarousel, NgbModal, NgbSlide} from "@ng-bootstrap/ng-bootstrap";
+import {RouterLink} from "@angular/router";
 
 @Component({
-  selector: 'app-home-page',
-  standalone: true,
-  imports: [
-    NgbCarousel,
-    NgbSlide
-  ],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+    selector: 'app-home-page',
+    standalone: true,
+    imports: [
+        NgbCarousel,
+        NgbSlide,
+        RouterLink
+    ],
+    templateUrl: './home-page.component.html',
+    styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
-
-  public modalService = inject(NgbModal);
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
-
-
+    public modalService = inject(NgbModal);
 }
